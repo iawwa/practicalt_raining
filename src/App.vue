9061a2d8b7defaf4d973eea4c0c880716bdfb70c
iawwa <752122134@qq.com>
2023-06-19 15:08:14 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import IFDemo from './components/ifDemo.vue'
+import ListDemo from './components/ListDemo.vue';
+</script>
+<template>
+  <HelloWorld></HelloWorld>
+  <IFDemo></IFDemo>
+  <ListDemo></ListDemo>
+</template>
