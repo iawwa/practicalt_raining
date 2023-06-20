f715dcd4025f77d5a984623935e61e66a4bc17c0
iawwa <752122134@qq.com>
2023-06-20 17:27:26 +0800

    修改

--- src/components/MainBody2.vue
+++ src/components/MainBody2.vue
@@ -2,7 +2,31 @@
 
   <div class="common-layout">
     <el-container class="layout-container-demo">
-      <el-header class="ori_el_header">Header</el-header>
+      <el-header class="ori_el_header">
+        <div style="width: 15%">
+          <User_pic></User_pic>
+        </div>
+        <div style="width: 80%">
+          mianbody_head
+
+        </div>
+        <div class="toolbar" style="width: 5%">
+        <el-dropdown>
+          <el-icon style="margin-right: 8px; margin-top: 1px">
+            <img src="src/assets/Icons/Utilities Sidebar.ico" style="width: 50px;height: 50px;" />
+          </el-icon>
+          <template #dropdown>
+            <el-dropdown-menu>
+              <el-dropdown-item>View</el-dropdown-item>
+              <el-dropdown-item>Add</el-dropdown-item>
+              <el-dropdown-item>Delete</el-dropdown-item>
+            </el-dropdown-menu>
+          </template>
+        </el-dropdown>
+        <span>--</span>
+        </div>
+
+      </el-header>
       <el-container >
         <el-aside width="200px" class="ori_el_aside">
           <el-scrollbar>
@@ -10,25 +34,18 @@
 
             <el-sub-menu index="1">
               <template #title>
-                <el-icon><message /></el-icon>课程中心
+                <el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Documents.ico"></el-icon>课程中心
               </template>
-              <el-menu-item-group>
-                <template #title>查看课程</template>
-                <el-menu-item index="1-1">操作1</el-menu-item>
-                <el-menu-item index="1-2">操作2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="1-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>Option4</template>
-                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
+              <el-menu-item index="1-3"><el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Home.ico"></el-icon>课程主页</el-menu-item>
+              <el-menu-item index="1-3"><el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Home.ico"></el-icon>课程主页</el-menu-item>
+              <el-menu-item index="1-3"><el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Home.ico"></el-icon>课程主页</el-menu-item>
+
+
             </el-sub-menu>
 
             <el-sub-menu index="2">
               <template #title>
-                <el-icon><icon-menu /></el-icon>Navigator Two
+                <el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Apps.ico"></el-icon>个人信息
               </template>
               <el-menu-item-group>
                 <template #title>Group 1</template>
@@ -46,8 +63,13 @@
 
             <el-sub-menu index="3">
               <template #title>
-                <el-icon><setting /></el-icon>Navigator Three
+                <el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Default App.ico"></el-icon>考试管理
               </template>
+              <el-menu-item-group>
+                <template #title><el-icon><img style="width: 30px;height: 30px" src="src/assets/Icons/Home.ico"></el-icon>测试管理主页</template>
+                <el-menu-item index="3-1">Option 1</el-menu-item>
+                <el-menu-item index="3-2">Option 2</el-menu-item>
+              </el-menu-item-group>
               <el-menu-item-group>
                 <template #title>Group 1</template>
                 <el-menu-item index="3-1">Option 1</el-menu-item>
@@ -66,30 +88,15 @@
         </el-scrollbar>
         </el-aside>
         <el-main class="ori_el_main"><el-container>
-          <el-header style="text-align: right; font-size: 12px">
-            <div class="toolbar">
-              <el-dropdown>
-                <el-icon style="margin-right: 8px; margin-top: 1px"
-                ><setting
-                /></el-icon>
-                <template #dropdown>
-                  <el-dropdown-menu>
-                    <el-dropdown-item>View</el-dropdown-item>
-                    <el-dropdown-item>Add</el-dropdown-item>
-                    <el-dropdown-item>Delete</el-dropdown-item>
-                  </el-dropdown-menu>
-                </template>
-              </el-dropdown>
-              <span>Tom</span>
-            </div>
-          </el-header>
-
           <el-main>
             <el-scrollbar>
               <el-table :data="tableData">
-                <el-table-column prop="date" label="Date" width="140" />
-                <el-table-column prop="name" label="Name" width="120" />
-                <el-table-column prop="address" label="Address" />
+                <el-table-column prop="id" label="id" width="50" />
+                <el-table-column prop="name" label="name" width="120" />
+                <el-table-column prop="sex" label="sex" width="50" />
+                <el-table-column prop="age" label="age" width="140" />
+                <el-table-column prop="phone" label="phone" width="120" />
+                <el-table-column prop="address" label="address"   />
               </el-table>
             </el-scrollbar>
           </el-main>
@@ -102,12 +109,17 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
-import { Menu as IconMenu, Message, Setting } from '@element-plus/icons-vue'
+import User_pic from "./user_pic.vue";
+
+
 
 const item = {
-  date: '2012-12-25',
-  name: 'Tom',
-  address: 'No. 189, Grove St, Los Angeles',
+  id:1,
+  name:"iawwa",
+  sex:"male",
+  age:"500",
+  phone:110,
+  address:"mars"
 }
 const tableData = ref(Array.from({ length: 20 }).fill(item))
 </script>
@@ -125,14 +137,14 @@ const tableData = ref(Array.from({ length: 20 }).fill(item))
 .ori_el_header
 {
   height: 100px;
-  background-color: pink;
+  background-color: #a63021;
   position: relative;
   color: var(--el-text-color-primary);
 }
 .ori_el_aside
 {
   color: var(--el-text-color-primary);
-  background-color: greenyellow;
+  background-color: #a63021;
 }
 .ori_el_main
 {
