8237d1daf0d316bab413256b9f704e2afc70a313
iawwa <752122134@qq.com>
2023-06-25 09:39:13 +0800

    更新

--- src/views/TestWeb.vue
+++ src/views/TestWeb.vue
@@ -1,40 +1,39 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
     <button @click="getData()">test axios 请求数据</button>
-    <p>这是请求到的数据{{testData.list}}</p>
+    <p>这是请求到的数据{{data}}</p>
   </div>
 </template>
-<script>
-import { reactive } from 'vue'
+<script lang="ts">
 import API from "../axios/request"
 export default{
-  name:'About',
-  setup(){
-    //数据
-    const testData = reactive({
-      list:[]
-    });
-
-    //测试请求方法
-    const getData = function(){
-      API({
-        url:'/selectExamination',
-        method:'get',
-        params:{
-          page:1,
-          limit:4
-        }
-      }).then((res)=>{
-        alert('请求成功!');
-        testData.list = res.data;
-      });
-    }
+  data(){
     return{
-      testData,
-      getData,
+      //定义变量
+      data:"",
     }
+  }, methods:
+{
+  //定义函数
+  getData()
+  {
+    API({
+      url:'/selectExamination',
+      method:'get',
+      params:{
+        page:1,
+        limit:4,
+      }
+    }).then((res) => {
+
+      this.data=res.data;
+    })
   }
+}
+
+
+
+
 
 }
 </script>
