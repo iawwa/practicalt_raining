403516798411e733c631443f64d243d18a195844
iawwa <752122134@qq.com>
2023-06-26 11:53:04 +0800

    更新

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,18 @@ const routes=[
         component:MainBody,
         children: [
             //这里是用来渲染主页面之下的部分
+            //班级管理
             {path:"/ClassInfo",component: () => import("@/views/ClassManager/ClassInfo.vue")},
             {path:"/CreateClass",component: () => import("@/views/ClassManager/CreateClass.vue")},
             {path:"/ManagerClass",component: () => import("@/views/ClassManager/ManagerClass.vue")},
 
-
+            //课程管理部分
             {path: "/CourseManager",component: () => import("@/views/CourseManager/CourseManager.vue")},
             {path: "/ListCourse", component: () => import("@/views/CourseManager/ListCourese.vue") },
             {path: "/MyCourse",component: () => import("@/views/CourseManager/MyCourse.vue") },
             {path: "/CreateCourse",component: () => import("@/views/CourseManager/CreateCourse.vue")},
 
+            // 个人中心
             {path: '/PersonInfo', component: ()=> import("@/views/PersonInfoManager/PersonInfo.vue") },
             {path: "/ResetPassword", component: () => import("@/views/PersonInfoManager/ResetPassword.vue")},
             {path: "/ResetHeadPic",component: () => import("@/views/PersonInfoManager/ResetHeadPic.vue")},
@@ -24,11 +26,12 @@ const routes=[
             {path: "/MyInfo",component: () => import("@/views/PersonInfoManager/MyInfo.vue")},
 
 
-
-            {path: "/Assessment",component: () => import("@/views/TestPaperManager/Assessment.vue")},
+            // 试卷考试管理
+            {path: "/TestPaperPark",component: () => import("@/views/TestPaperManager/TestPaperPark.vue")},
             {path: "/CreateTestPaper",component: () => import("@/views/TestPaperManager/CreateTestPaper.vue")},
             {path:"/ListTestPaper",component: () => import("@/views/TestPaperManager/ListTestPaper.vue")},
             {path:"/DoTestPaper",component: () => import("@/views/TestPaperManager/DoTestPaper.vue")},
+            {path:"/IHaveDoTestPaper",component: () => import("@/views/TestPaperManager/IHaveDoTestPaper.vue")}
 
         ],
     },
